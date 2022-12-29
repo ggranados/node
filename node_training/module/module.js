6f8b60a923c0d99828030e44e1c25095323b3962
Guillermo Granados <guillermo.granados@outlook.com>
2022-12-29 19:28:30 -0300

    [node basic] added module export example

---
+++ node_training/module/module.js
@@ -0,0 +1,13 @@
+let counter = function(arr){
+    console.log(`Total elements count ${arr.length}`)
+}
+
+let add = function( num1, num2){
+    return num1 + num2;
+}
+
+let pi = 3.1416;
+
+module.exports.counter = counter;
+module.exports.add = add;
+module.exports.pi = pi;
---
+++ node_training/module/moduletest.js
@@ -0,0 +1,7 @@
+let myStuff = require("./module");
+
+myStuff.counter(["HR", "IT", "DEVOPS"]);
+console.log(`Sum is ${myStuff.add(40,60)}`);
+console.log(`Sum using PI ${myStuff.add(40,myStuff.pi)}`);
+
+
