90e7fff4758a9c4427bf5284fb7d3975d76d0029
Guillermo Granados <guillermo.granados@outlook.com>
2022-12-29 19:27:57 -0300

    [node basic] added global objects console log

---
+++ node_training/fundamentals/globalobjects.js
@@ -0,0 +1,8 @@
+console.log("Dir name: " + __dirname)
+console.log("File name: " + __filename)
+console.log("Process properties and Function")
+console.log(`Process ${process.arch}`);
+console.log(`Process ${process.pid}`);
+console.log(`Process ${process.platfom}`);
+console.log(`Process ${process.version}`);
+console.log(`Process ${process.cwd()}`);
